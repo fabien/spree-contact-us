ea467298a77c0f1412027ea1d032c06d212a2a69
Fabien Franzen <info@atelierfabien.be>
2011-07-28 13:32:59 +0200

    Fixed hooks deprecation using Deface

--- lib/spree_contact_us_hooks.rb
+++ lib/spree_contact_us_hooks.rb
@@ -1,19 +1,16 @@
-class SpreeContactUsHooks < Spree::ThemeSupport::HookListener
-  insert_after(:admin_tabs) do
-    %[<%= tab(:inquiries) %>]
-  end
+Deface::Override.new(:virtual_path => "layouts/admin",
+                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :text => "<%= tab(:inquiries) %>")
 
-  insert_after(:admin_configurations_sidebar_menu) do
-    %[<li<%= ' class="active"' if controller.controller_name == 'inquiries' %>><%= link_to t("inquiries_settings"), admin_inquiries_settings_path %></li>]
-  end 
+Deface::Override.new(:virtual_path => "admin/shared/_configuration_menu",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     :text => %[<li<%= ' class="active"' if controller.controller_name == 'inquiries' %>><%= link_to t("inquiries_settings"), admin_inquiries_settings_path %></li>])
 
-  insert_after(:admin_configurations_menu) do
-    %[
+Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text => %[
       <tr>
         <td><%= link_to t("inquiries_settings"), admin_inquiries_settings_path %></td>
         <td><%= t("inquiries_settings_description") %></td>
       </tr>
-    ] 
-  end
-
-end
+    ])
